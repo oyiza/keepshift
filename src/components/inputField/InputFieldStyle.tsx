b39399459d0cd3acc9b260945ff717d2a96bab1e
Meet Pandya <pandyama2@gmail.com>
2023-04-29 10:39:32 -0600

    new input field component created

---
+++ src/components/inputField/InputFieldStyle.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  input: {
+    fontSize: 19,
+    height: 50,
+    width: '100%',
+    margin: 7,
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 10,
+    textAlign: 'center' // placeholder alignment
+  }
+})
